84d6420a83103b08e788d722c4676bb7b220f877
unknown <martincodesthings@gmail.com>
2025-04-25 16:03:44 -0500

    added explicity static folder reference

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from .routes import main_bp
 from .utils.data_loader import read_gcs_csv
 
 def create_app():
-    app = Flask(__name__, template_folder="templates")
+    app = Flask(__name__, template_folder="templates", static_folder='static')
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.secret_key = 'mliu'  # Should move to config.py and use env var
     
